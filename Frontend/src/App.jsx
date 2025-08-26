30c14821db2150f2e75a41afc395e4a466b9d282
simplysaad <saadidris70@gmail.com>
2025-08-26 18:39:01 +0100

    initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import UploadForm from "./Components/UploadForm";
+
+const App = () => {
+  return (
+    <div className="container min-w-screen min-h-screen bg-green-500 flex flex-col justify-center items-center">
+      hello world
+    </div>
+  );
+};
+
+export default App;
