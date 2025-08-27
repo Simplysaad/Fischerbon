f71a0e7995da474dbea2fc5932de1f4535543db5
simplysaad <saadidris70@gmail.com>
2025-08-27 08:40:24 +0100

    fix: made some changes

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import express from "express";
-import connectDB from "./Config/db";
+import connectDB from "./Config/db.js";
 
 const app = express();
 
 app.listen(process.env.PORT, (err) => {
-  if (!err) connectDB();
+  if (!err) {
+    connectDB();
+    console.log(`sever live at http://localhost:${process.env.PORT}`);
+  }
 });
-
