0c2bc88ec1250e04b4f4ec54fcae72c737425aee
simplysaad <saadidris70@gmail.com>
2025-09-27 23:45:17 +0100

    chore: changed the url of self ping

--- Backend/src/Cron/keep-alive.js
+++ Backend/src/Cron/keep-alive.js
@@ -5,7 +5,7 @@ export default function keepAlive() {
   if (["prod", "production"].includes(process?.env?.NODE_ENV || "")) {
     cron.schedule("*/5 * * * *", async () => {
       try {
-        await axios.get(`${process.env.RENDER_DOMAIN}`);
+        await axios.get(`${process.env.BASE_URL}`);
         console.log("Self-ping sent at", new Date().toLocaleTimeString());
       } catch (err) {
         console.error("Self-ping failed:", err.message);
